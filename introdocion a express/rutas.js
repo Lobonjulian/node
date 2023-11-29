1f00bccad9a3f4e5a573ce449357bb2a456dd1f9
Julian Lobon <julitolos6@gmail.com>
2023-11-29 11:53:44 -0500

    FIX

--- introdocion a express/rutas.js
+++ introdocion a express/rutas.js
@@ -1,5 +1,7 @@
 const http = require("node:http");
 
+const dittoJSON = require("./pokemon/ditto");
+
 const processRequest = (req, res) => {
   const { method, url } = req;
 
@@ -12,8 +14,7 @@ const processRequest = (req, res) => {
         default:
           res.statusCode = 404;
           res.setHeader("Content-Type", "text/html; charset=utf-8");
-          return res.end("<h1>no se encontro la ruta</h1>");
-        case "/about":
+          return res.end("<h1>no se encontró la ruta 404</h1>");
       }
 
     case "POST":
@@ -30,11 +31,20 @@ const processRequest = (req, res) => {
               "Content-type": "application/json; charset=utf-8",
             });
 
+            data.timestamp = Date.now();
             res.end(JSON.stringify(data));
           });
+
+          break;
       }
+    default:
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain; charset=utf-8");
+      return res.end("no se encontró la ruta 404");
   }
 };
 
 const server = http.createServer(processRequest);
-server.listen("servicio en el puerto http://localhost:3000");
+server.listen(3002, () => {
+  console.log("servicio en el puerto http://localhost:3002");
+});
